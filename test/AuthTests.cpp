a23f584e6e273d0cbfad0d1eded358a6223ecf01
Philipp Maluta <fil.the.ensoreus@gmail.com>
2013-08-20 11:11:43 +0300

    Built gtest/gmock with C++11 support and patched gtest-port.h added timeouts with thread feature of C++11

--- test/AuthTests.cpp
+++ test/AuthTests.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "AuthLayer.hpp"
@@ -24,62 +28,69 @@ public:
     MOCK_METHOD0(onAuthorizationSuccessful, void());
 };
 
- TEST_F(AuthTestSuite, TestServerURLSetupCorrectly){
-     ASSERT_TRUE(_authLayer != nullptr);
-     EXPECT_EQ(_authLayer->getServerURL(), SERVER_URL);
- }
+TEST_F(AuthTestSuite, TestServerURLSetupCorrectly){
+    ASSERT_TRUE(_authLayer != nullptr);
+    EXPECT_EQ(_authLayer->getServerURL(), SERVER_URL);
+}
 
- TEST_F(AuthTestSuite, TestServerDenyWithAllInvalidCredentionals){
-     using ::testing::Mock;
-     MockAuthLayerDelegate *mockAuthLayerDelegate = new MockAuthLayerDelegate();
+void timeout(){
+    this_thread::sleep_for(chrono::seconds(2));
+}
 
-     _authLayer->setLogin(INVALID_LOGIN);
-     _authLayer->setPassword(INVALID_PASSWORD);
-     _authLayer->setDelegate(mockAuthLayerDelegate);
-      EXPECT_CALL(*mockAuthLayerDelegate, onAuthorizationFailed()).Times(1);
-     _authLayer->authorize();
 
-     Mock::VerifyAndClearExpectations(mockAuthLayerDelegate);
-     delete mockAuthLayerDelegate;
- }
+TEST_F(AuthTestSuite, TestServerDenyWithAllInvalidCredentionals){
+    using ::testing::Mock;
+    MockAuthLayerDelegate *mockAuthLayerDelegate = new MockAuthLayerDelegate();
+    thread timeoutThread(timeout);
+    _authLayer->setLogin(INVALID_LOGIN);
+    _authLayer->setPassword(INVALID_PASSWORD);
+    _authLayer->setDelegate(mockAuthLayerDelegate);
+    EXPECT_CALL(*mockAuthLayerDelegate, onAuthorizationFailed()).Times(1);
+    _authLayer->authorize();
+    timeoutThread.join();
+    Mock::VerifyAndClearExpectations(mockAuthLayerDelegate);
+    delete mockAuthLayerDelegate;
+}
 
- TEST_F(AuthTestSuite, TestServerDenyWithInvalidPassword){
-     using ::testing::Mock;
-     MockAuthLayerDelegate *mockAuthLayerDelegate = new MockAuthLayerDelegate();
+TEST_F(AuthTestSuite, TestServerDenyWithInvalidPassword){
+    using ::testing::Mock;
+    MockAuthLayerDelegate *mockAuthLayerDelegate = new MockAuthLayerDelegate();
+    thread timeoutThread(timeout);
+    _authLayer->setLogin(VALID_LOGIN);
+    _authLayer->setPassword(INVALID_PASSWORD);
+    _authLayer->setDelegate(mockAuthLayerDelegate);
+    EXPECT_CALL(*mockAuthLayerDelegate, onAuthorizationFailed()).Times(1);
+    _authLayer->authorize();
+    timeoutThread.join();
+    Mock::VerifyAndClearExpectations(mockAuthLayerDelegate);
+    delete mockAuthLayerDelegate;
+}
 
-     _authLayer->setLogin(VALID_LOGIN);
-     _authLayer->setPassword(INVALID_PASSWORD);
-     _authLayer->setDelegate(mockAuthLayerDelegate);
-     EXPECT_CALL(*mockAuthLayerDelegate, onAuthorizationFailed()).Times(1);
-     _authLayer->authorize();
-     Mock::VerifyAndClearExpectations(mockAuthLayerDelegate);
-     delete mockAuthLayerDelegate;
- }
+TEST_F(AuthTestSuite, TestServerAcceptWithValidCredentions){
+    using ::testing::Mock;
+    MockAuthLayerDelegate *mockAuthLayerDelegate = new MockAuthLayerDelegate();
+    thread timeoutThread(timeout);
+    _authLayer->setLogin(VALID_LOGIN);
+    _authLayer->setPassword(VALID_PASSWORD);
+    _authLayer->setDelegate(mockAuthLayerDelegate);
+    EXPECT_CALL(*mockAuthLayerDelegate, onAuthorizationSuccessful()).Times(1);
 
- TEST_F(AuthTestSuite, TestServerAcceptWithValidCredentions){
-     using ::testing::Mock;
-     MockAuthLayerDelegate *mockAuthLayerDelegate = new MockAuthLayerDelegate();
+    _authLayer->authorize();
+    timeoutThread.join();
+    Mock::VerifyAndClearExpectations(mockAuthLayerDelegate);
+    delete mockAuthLayerDelegate;
+}
 
-     _authLayer->setLogin(VALID_LOGIN);
-     _authLayer->setPassword(VALID_PASSWORD);
-     _authLayer->setDelegate(mockAuthLayerDelegate);
-     EXPECT_CALL(*mockAuthLayerDelegate, onAuthorizationSuccessful()).Times(1);
+TEST_F(AuthTestSuite, TestWhetherAuthPassesWitoutDelegateSetup){
+    _authLayer->setLogin(VALID_LOGIN);
+    _authLayer->setPassword(VALID_PASSWORD);
+    _authLayer->setDelegate(nullptr);
+    EXPECT_NO_FATAL_FAILURE(_authLayer->authorize());
+}
 
-     _authLayer->authorize();
-     Mock::VerifyAndClearExpectations(mockAuthLayerDelegate);
-     delete mockAuthLayerDelegate;
- }
-
- TEST_F(AuthTestSuite, TestWhetherAuthPassesWitoutDelegateSetup){
-     _authLayer->setLogin(VALID_LOGIN);
-     _authLayer->setPassword(VALID_PASSWORD);
-     _authLayer->setDelegate(nullptr);
-     EXPECT_NO_FATAL_FAILURE(_authLayer->authorize());
- }
-
- TEST_F(AuthTestSuite, TestIsAuthorizedIsSetupOnSuccess){
-     _authLayer->setLogin(VALID_LOGIN);
-     _authLayer->setPassword(VALID_PASSWORD);
-     _authLayer->authorize();
-     EXPECT_TRUE(_authLayer->isAuthorized());
- }
+TEST_F(AuthTestSuite, TestIsAuthorizedIsSetupOnSuccess){
+    _authLayer->setLogin(VALID_LOGIN);
+    _authLayer->setPassword(VALID_PASSWORD);
+    _authLayer->authorize();
+    EXPECT_TRUE(_authLayer->isAuthorized());
+}
