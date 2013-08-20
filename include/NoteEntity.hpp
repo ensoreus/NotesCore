157774b97304a38e2a97b5bed9c13a003ff95818
Philipp Maluta <fil.the.ensoreus@gmail.com>
2013-08-20 03:50:35 +0300

    entity GREEN

--- include/NoteEntity.hpp
+++ include/NoteEntity.hpp
@@ -1,10 +1,17 @@
 #ifndef NOTEENTITY_HPP
 #define NOTEENTITY_HPP
+#include <string>
+
+using namespace std;
 
 class NoteEntity
 {
 public:
     NoteEntity();
+    virtual void setBody(string body){_body = body;}
+    virtual string getBody() const {return _body;}
+private:
+    string _body;
 };
 
 #endif // NOTEENTITY_HPP
--- test/NoteEntityTests.cpp
+++ test/NoteEntityTests.cpp
@@ -17,5 +17,7 @@ class NoteEntityTestSuite : public ::testing::Test{
 
 
 TEST_F(NoteEntityTestSuite, TestNoteHaveBody){
-
+    string body("body1");
+    _note->setBody(body);
+    ASSERT_EQ(_note->getBody(), body);
 }
